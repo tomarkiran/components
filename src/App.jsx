947aaccf59d460784deb314603accc4ed921aa2f
Kiran Tomar <tomarkiran559@gmail.com>
2025-08-22 18:01:05 +0530

    componentsOne

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Cards from './Components/Cards'
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <Cards name="Kiran">
+
+        </Cards>
+      </div>
+      
+  )
+}
+
+export default App
